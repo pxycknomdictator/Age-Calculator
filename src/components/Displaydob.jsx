453f1041ec4f188b858f421342adb52d52991b32
Noman <pxycknomdictator@outlook.com>
2024-05-17 15:46:13 +0500

    improve design

--- src/components/Displaydob.jsx
+++ src/components/Displaydob.jsx
@@ -3,10 +3,10 @@ import React from "react";
 const Displaydob = ({ date, age }) => {
   return (
     <div className="flex items-center gap-2 mb-2">
-      <span className="text-5xl font-extrabold italic text-purple sm:text-[3.4rem] md:text-[3.5rem] lg:text-7xl">
+      <span className="text-5xl font-extrabold italic text-purple sm:text-[3.4rem] md:text-[3.5rem] lg:text-7xl xl:text-[5rem]">
         {age}
       </span>
-      <h1 className="text-5xl font-extrabold italic sm:text-[3.4rem] md:text-[3.5rem] lg:text-7xl">
+      <h1 className="text-5xl font-extrabold italic sm:text-[3.4rem] md:text-[3.5rem] lg:text-7xl xl:text-[5rem]">
         {date}
       </h1>
     </div>
